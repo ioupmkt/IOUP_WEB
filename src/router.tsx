90f06e163a5b59d6d6baa4026ec3a19c7c0b9b72
Iuri Ramos <iuri.ramos41@gmail.com>
2023-03-16 21:23:39 -0300

    :sparkles: TEST ROUTES BROWSER

--- src/router.tsx
+++ src/router.tsx
@@ -12,7 +12,7 @@ const routes = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/portfolio",
+        path: "portfolio",
         element: <Portfolio />,
       },
     ],
